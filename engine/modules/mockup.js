621cd20a23caba8b8191ae210b78abb1ca8a6066
simonwallin <simon.wallin@eyevinn.se>
2018-03-16 17:45:31 +0100

    added mock ad response

--- engine/modules/mockup.js
+++ engine/modules/mockup.js
@@ -1,5 +1,87 @@
 const errs = require('restify-errors');
 const debug = require('debug')('module-mockup');
+const xml = require("xml");
+const VAST = require('vast-xml');
+
+const selectAd = (vast, params) => {
+  if(params.profile === "1"){
+    createAd(vast, preroll, params)
+    createAd(vast, volvo, params)
+    createAd(vast, postroll, params)
+  }else if(params.profile === "2"){
+    createAd(vast, preroll, params)
+    createAd(vast, umeaenergi, params)
+    createAd(vast, postroll, params)
+  }
+}
+
+const createAd = (vast, adObject, params) => {
+  const ad = vast.attachAd({
+    id: adObject.id,
+    structure: 'inline',
+    AdTitle: adObject.title,
+    AdSystem: {
+      name: 'mock',
+      version: '1.0'
+    }
+  });
+
+  const creative = ad.attachCreative('Linear', {
+    AdParameters : '<xml></xml>',
+    Duration: adObject.duration,
+  });
+  if(adObject.impression){
+    ad.attachImpression({
+      url: params.trackingUrl + '/track?AdId='+ adObject.id +'&Profile=' + params.profile + '&Channel=' + params.channel
+    })
+  }
+  creative.attachMediaFile(adObject.url, {
+    type: "video/mp4",
+    id: adObject.id,
+    bitrate: "11421",
+    minBitrate: "11421",
+    maxBitrate: "11421",
+    width: "1280",
+    height: "720",
+    scalable: "true",
+    maintainAspectRatio: "true",
+    codec: ""
+  });
+}
+
+const preroll = {
+  id: "preroll-video",
+  title: "preroll",
+  duration: "00:00:10",
+  url: 'http://cdn.lab.eyevinn.technology/ads/TV4-PreRoll-Reklam.mp4',
+  impression: false,
+}
+
+const volvo = {
+  id: "video-volvo-impression",
+  title: "VolvoS90LuxurySedanSongoftheOpenRoad-30s",
+  duration: "00:00:30",
+  url: "http://cdn.lab.eyevinn.technology/ads/VolvoS90LuxurySedanSongoftheOpenRoad-30s.mp4",
+  impression: true,
+}
+
+const umeaenergi = {
+  id: "video-UmeaEnergi-impression",
+  title: "video-ad-UmeaEnergi",
+  duration: "00:00:30",
+  url: "http://cdn.lab.eyevinn.technology/ads/UmeaEnergi-Bredband-30s.mp4",
+  impression: true,
+}
+
+const postroll = {
+  id: "video-post",
+  title: "video-postroll",
+  url: "http://cdn.lab.eyevinn.technology/ads/TV4-PostRoll-Reklam.mp4",
+  duration: "00:00:10",
+  impression: false,
+}
+
+const mockParams = {profile: ["1", "2"][Math.floor(Math.random())]}
 
 class MockupExchange {
   constructor() {
@@ -7,9 +89,12 @@ class MockupExchange {
   }
 
   handleRequest(req, res, next) {
+    res.set('content-type', 'application/xml');
     debug(`Handle request`);
     this.generateMockupResponse().then(payload => {
-      res.send(payload);
+      const vast = new VAST({version: "4.0"});
+      selectAd(vast, mockParams)
+      res.send(vast.xml({ pretty : true, indent : '  ', newline : '\n' }));
       next();
     }).catch(err => {
       next(this._errorHandler(err));
